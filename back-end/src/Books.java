b4a3f91b7ce4acc8da258c1d7537adbdbf4c7077
unknown <Gusabary@126.com>
2019-03-07 10:46:58 +0800

    feat: finish '/api/manage/book' GET method basically

--- back-end/src/Books.java
+++ back-end/src/Books.java
@@ -80,15 +80,105 @@ public class Books extends HttpServlet {
 
             int rs = stmt.executeUpdate(sql);
 
-            /*if (userId==0) {
-                response.setStatus(500);
-                out.println("Error: wrong username or password!");
-            }
-            if (validity==0){
-                response.setStatus(403);
-                out.println("Error: you are forbidden!");
-            }*/
             out.println("Add book successfully!");
+        } catch(SQLException se) {
+            // 处理 JDBC 错误
+            se.printStackTrace();
+            out.println(se);
+            out.println("JDBCError");
+        } catch(Exception e) {
+            // 处理 Class.forName 错误
+            e.printStackTrace();
+            out.println("ForNameError");
+        }finally{
+            // 最后是用于关闭资源的块
+            try{
+                if(stmt!=null)
+                    stmt.close();
+            }catch(SQLException se2){
+            }
+            try{
+                if(conn!=null)
+                    conn.close();
+            }catch(SQLException se){
+                se.printStackTrace();
+            }
+        }
+    }
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Connection conn = null;
+        Statement stmt = null;
+
+        response.setContentType("application/json;charset=UTF-8");
+
+        BufferedReader br=request.getReader();
+        String str,wholeStr="";
+        while ((str=br.readLine())!=null){
+            wholeStr+=str;
+        }
+        PrintWriter out = response.getWriter();
+
+        try{
+            // 注册 JDBC 驱动器
+            Class.forName("com.mysql.jdbc.Driver");
+
+            // 打开一个连接
+            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+
+            // 执行 SQL 查询
+            stmt = conn.createStatement();
+            String sql;
+            sql="SELECT `bookId`, `bookName`, `author`, `coverPath`, `ISBN`, `storage`, `price` from `book`";
+
+            ResultSet rs = stmt.executeQuery(sql);
+
+            int bookId=0;
+            String bookName="",author="",coverPath="",ISBN="";
+            int storage=0;
+            float price=0;
+            out.println("{\"books\":[");
+            if (rs.next()){
+                bookId = rs.getInt("bookId");
+                bookName = rs.getString("bookName");
+                author=rs.getString("author");
+                coverPath = rs.getString("coverPath");
+                ISBN=rs.getString("ISBN");
+                storage=rs.getInt("storage");
+                price=rs.getFloat("price");
+                out.println(
+                        "{" +
+                                "\"bookId\":"+bookId +
+                                ",\"bookName\":\""+bookName+
+                                "\",\"author\":\""+author+
+                                "\",\"coverPath\":\""+coverPath+
+                                "\",\"ISBN\":\""+ISBN+
+                                "\",\"storage\":"+storage+
+                                ",\"price\":"+price+
+                                "}"
+                );
+            }
+            while(rs.next()) {
+                bookId = rs.getInt("bookId");
+                bookName = rs.getString("bookName");
+                author=rs.getString("author");
+                coverPath = rs.getString("coverPath");
+                ISBN=rs.getString("ISBN");
+                storage=rs.getInt("storage");
+                price=rs.getFloat("price");
+                out.println(
+                        ",{" +
+                                "\"bookId\":"+bookId +
+                                ",\"bookName\":\""+bookName+
+                                "\",\"author\":\""+author+
+                                "\",\"coverPath\":\""+coverPath+
+                                "\",\"ISBN\":\""+ISBN+
+                                "\",\"storage\":"+storage+
+                                ",\"price\":"+price+
+                                "}"
+                );
+            }
+            rs.close();
+            out.println("]}");
         } catch(SQLException se) {
             // 处理 JDBC 错误
             se.printStackTrace();
