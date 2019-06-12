6158b051cf800fc0e2c483a1db635756a6ae39ad
unknown <Gusabary@126.com>
2019-06-12 11:50:19 +0800

    really final

--- back-end/src/main/java/com/example/demo/secret/JWTService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.secret;
-
-public class JWTService {
-
-    public static final int expirationTime = 1 * 60 * 60 * 1000;  //1 hour
-
-    public static final String secret = "sone0805";
-
-}
