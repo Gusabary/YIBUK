9aa715f8b2e34806429851e406790a179b4f6e36
unknown <Gusabary@126.com>
2019-06-29 16:56:43 +0800

    feat: can show comments at front-end now

--- front-end/src/Components/Books/BookContent.js
+++ front-end/src/Components/Books/BookContent.js
@@ -1,13 +1,47 @@
 import React from 'react'
-import { withStyles, Typography, Divider } from '@material-ui/core'
+import { withStyles, Typography, Divider, Paper } from '@material-ui/core'
 import BookInfoList from './BookInfoList'
 import ModeButtons from './ModeButtons/index'
 
-const parse = content => {
+const parseIntroduction = content => {
     const splitedContent = content.split('\n');
     return splitedContent.map(para =>
         <p>{para}</p>
     )
+}
+
+const CommentStyle = {
+    borderWidth: 1,
+    borderStyle: "solid",
+    borderColor: "purple",
+    marginTop: 10,
+    padding: 6,
+    paddingLeft: 6,
+    paddingRight: 6,
+}
+
+const parseComments = comments => {
+    if (comments == null)
+        return null
+    return comments.map(comment =>
+        <Paper style={CommentStyle}>
+            <Typography variant="h6">
+                #{comment.userId} &nbsp;
+                {parseTime(comment.time)}
+            </Typography>
+            <Typography variant="body1">
+                {comment.content}
+            </Typography>
+            {parseComments(comment.followup)}
+        </Paper>
+    )
+}
+
+const parseTime = time => {
+    let parsedTime = time.substr(0, 9);
+    parsedTime = parsedTime.concat(" ");
+    parsedTime = parsedTime.concat(time.substr(11, 8));
+    return parsedTime;
 }
 
 const styles = theme => ({
@@ -51,7 +85,16 @@ class BookContent extends React.Component {
                         </Typography>
                         <Divider />
                         <br />
-                        {parse(book.introduction)}
+                        {parseIntroduction(book.introduction)}
+                    </div>
+
+                    <div style={{ flex: 1, marginTop: 30, width: 1000 }}>
+                        <Typography variant='h4'>
+                            Comments
+                        </Typography>
+                        <Divider />
+                        <br />
+                        {parseComments(book.comments)||"No comments yet."}
                     </div>
                 </div>
                 <br />
